1b385b59bcc751504e1927b33f611f073a3680c5
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2021-12-02 15:30:04 -0300

    Requisito 1 OK

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,13 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(p1, p2) {
+  if (p1 === true && p2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea() {
