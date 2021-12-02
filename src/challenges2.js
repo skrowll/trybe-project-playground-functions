7e994560e7214f80a71e0dbaa39f0b93fad58086
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2021-12-02 20:32:42 -0300

    começando requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(NomeTech, name) {
+  let object = {
+    tech:
+    name:
+  };
+  for (let index = 0; index < NomeTech.length ; index ++) {
+    for (let key in xxx) {
+
+    }
+  }
 }
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
