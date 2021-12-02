75a76c02f70f2a14d1ec3d43c139bec5db849fe4
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2021-12-02 16:21:20 -0300

    Requisito 5 OK

--- src/challenges.js
+++ src/challenges.js
@@ -28,9 +28,10 @@ function concatName(array) {
 //console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
+//console.log(footballPoints(14, 8));
 
 // Desafio 6
 function highestCount() {
