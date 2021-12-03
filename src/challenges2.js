3444532e0977dfdc110d2cd28f79b226b50931ba
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2021-12-03 20:20:21 -0300

    Requisito 10 OK

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,16 @@
 // Desafio 10
 function techList(NomeTech, name) {
-  let object = {
-    tech:
-    name:
-  };
-  for (let index = 0; index < NomeTech.length ; index ++) {
-    for (let key in xxx) {
-
-    }
+  let arrayObjects = [];
+  let NomeTechOrder = NomeTech.sort();
+  if (NomeTech.length === 0) {
+    return "Vazio!"
   }
+  for (let index = 0; index < NomeTechOrder.length; index ++) {
+    arrayObjects.push({tech: NomeTech[index], name: name});
+  }
+  return arrayObjects;
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
+//console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
 function generatePhoneNumber() {
