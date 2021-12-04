01dd2901f0506ae47b21dc91151de9c93486d44f
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2021-12-04 00:33:53 -0300

    Requisito 11 OK

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,9 +13,31 @@ function techList(NomeTech, name) {
 //console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"))
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let phoneNumber = "";
+  let flag = false;
+  for (index of numbers) {
+    let count = 0;
+    for(index2 of numbers) {
+      if (index2 === index) {
+        count = count + 1;
+      }
+    }
+    if (count > 2 || index > 9 || index < 0) {
+      flag = true;
+      break;
+    }
+  }
+  if (numbers.length !== 11){
+    return "Array com tamanho incorreto."
+  } else if (flag === true) {
+    return "não é possível gerar um número de telefone com esses valores"
+  } else if (numbers.length === 11 && flag === false) {
+    phoneNumber = ("(" + numbers[0] + numbers[1] + ") " + numbers[2] + numbers[3] + numbers[4] +numbers[5] + numbers[6] + "-" + numbers[7] + numbers[8] + numbers[9] + numbers[10]);
+    return phoneNumber;
+  } 
 }
+//console.log(generatePhoneNumber([1, 2, 3, 4, 5, 2, 3, 9, 6, 1, 9]))
 
 // Desafio 12
 function triangleCheck() {
